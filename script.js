f6440891c6ca9a8c0bcfa44355a82dbbfe431330
= <=>
2023-06-09 21:37:20 -0700

    addMovieToPagefunction

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function performSearch() {
 }
 
 function searchMovies(searchTerm) {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchTerm}`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${secrets.API_KEY}&query=${searchTerm}`;
 
     fetch(url)
         .then(response => response.json())
@@ -32,4 +32,30 @@ function searchMovies(searchTerm) {
             });
         })
         .catch(error => console.error('Error:', error));
+}
+function addMovieToPage(movie) {
+    const movieList = document.querySelector("#movie-list");
+
+    const movieItem = document.createElement("li");
+    const movieTitle = document.createElement("h2");
+    const movieImage = document.createElement("img");
+    const favoriteButton = document.createElement("button");
+    const watchlistButton = document.createElement("button");
+
+    movieTitle.textContent = movie.title;
+    movieImage.src = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+    favoriteButton.textContent = "Add to Favorites";
+    watchlistButton.textContent = "Add to Watchlist";
+
+    favoriteButton.classList.add("button", "favorite");
+    watchlistButton.classList.add("button", "watchlist");
+
+    favoriteButton.addEventListener("click", () => addToFavorites(movie));
+    watchlistButton.addEventListener("click", () => addToWatchlist(movie));
+
+    movieItem.appendChild(movieTitle);
+    movieItem.appendChild(movieImage);
+    movieItem.appendChild(favoriteButton);
+    movieItem.appendChild(watchlistButton);
+    movieList.appendChild(movieItem);
 }
